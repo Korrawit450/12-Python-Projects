1f206a5ffd93864d2c7ed76b2288fa4b5bf613d8
Korrawit450 <58800719+Korrawit450@users.noreply.github.com>
2024-06-29 20:48:11 +0700

    Add files via upload

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -15,20 +15,39 @@ def hangman():
     alphabet = set(string.ascii_uppercase)
     used_letters = set() # what the user has guessed
 
+    lives = 6
+
     # getting user input
-    user_letter = input('Guess a letter: ').upper()
-    if user_letter in alphabet - used_letters:
-        used_letters.add(user_letter)
-        if user_letter in word_letters:
-            word.letters.remove(user_letter)
+    while len(word_letters) > 0 and lives > 0:
+        # letters used
+        # ' '.join(['a', 'b', 'cd']) --> 'a b cd'
+        print('You have', lives, 'lives left and you have used these letters: ', ' '.join(used_letters))
 
-    elif user_letter in used_letters:
-        print('You have already used that character. Please try again')
-    
-    else:
-        print('Invalid character. Please try again')
+        # what current word is (ie W - R D)
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+        print('Current word: ', ' '.join(word_list))
+
+        user_letter = input('Guess a letter: ').upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word.letters.remove(user_letter)
+
+            else:
+                lives = lives - 1 # take away a life if wrong
+                print('Letter is not in word.')
+
+        elif user_letter in used_letters:
+            print('You have already used that character. Please try again')
         
+        else:
+            print('Invalid character. Please try again')
 
+    # gets here when len(word_letters) == 0 OR when lives == 0
+    if lives == 0:
+        print(lives_visual_dict[lives])
+        print('You died, sorry. The word was', word)
+    else:
+        print('YAY! You guessed the word', word, '!!')
 
-user_input = input('Type something: ')
-print(user_input)
+hangman()
