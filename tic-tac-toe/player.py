3d863bafafc54c62ebfe9da159b92c0999c454c1
Korrawit450 <58800719+Korrawit450@users.noreply.github.com>
2024-07-07 20:21:05 +0700

    Add files via upload

--- tic-tac-toe/player.py
+++ tic-tac-toe/player.py
@@ -1,1 +1,20 @@
 import math
+import random
+
+class Player:
+    def _init_(self, letter):
+        # letter is x and o
+        self.letter = letter
+
+    # we want all players to get their next move
+    def get_move(self, game):
+        pass
+
+class RandomComputerPlayer(Player):
+    def _init_(self, letter):
+        super()._init_(letter)
+
+    def get_move(self, game):
+        pass
+
+class HumanPlayer(Player):
