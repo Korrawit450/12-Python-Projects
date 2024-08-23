dfa4981ee7e7a39979c3f1bbd75c485cc4eea5d3
Korrawit450 <58800719+Korrawit450@users.noreply.github.com>
2024-08-23 21:28:46 +0700

    Add files via upload

--- tic-tac-toe/player.py
+++ tic-tac-toe/player.py
@@ -39,4 +39,14 @@ class HumanPlayer(Player):
             except ValueError:
                 print('Invalid square. Try again.')
         return val
-                
+
+def GeniusComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        if len(game.available_moves()) == 9:
+            square = random.choice(game.available_moves()) # randomly choose one
+        else:
+            # get the square based off the minimax algorithm
+            square = self.minimax(game, self.letter)
