0352e40d4fff6eb24b6cda61be955824b0c20e36
Korrawit450 <58800719+Korrawit450@users.noreply.github.com>
2024-09-01 23:00:30 +0700

    Add files via upload

--- tic-tac-toe/player.py
+++ tic-tac-toe/player.py
@@ -59,3 +59,9 @@ def GeniusComputerPlayer(Player):
         # first, we want to check if the previous move is a winner
         # this is our base case
         if state.current_winner == other_player:
+            return {'position': None, 
+                    'score': 1 * (state.num_empty_squares() + 1) if other_player == max_player else -1 * (
+                        state.num_empty_squares() + 1)
+                    }
+        
+        elif
