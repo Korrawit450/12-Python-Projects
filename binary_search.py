8217c436cff11711f7e85425cf5ceaeb4f7054ef
Korrawit450 <58800719+Korrawit450@users.noreply.github.com>
2024-10-03 19:56:44 +0700

    Add files via upload

--- binary_search.py
+++ binary_search.py
@@ -5,3 +5,16 @@
 # naive search: scan entire list and ask if its equal to the target
 # if yes, return the index
 # if no, then return -1
+def naive_search(l, target):
+    # example l = [1, 3, 10, 12]
+    for i in range(len(l)):
+        if l[i] == target:
+            return i
+    return -1
+
+
+# binary search uses divide and conquer!
+# we will leverage the fact that our list is SORTED
+def binary_search(l, target):
+    # example l = [1, 3, 5, 10, 12] # should return 3
+    midpoint = (len(l)) // 2 # 2
