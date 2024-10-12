032cc24f3d5612f52b40adb6c47904df1f522893
Korrawit450 <58800719+Korrawit450@users.noreply.github.com>
2024-10-12 14:50:13 +0700

    Add files via upload

--- binary_search.py
+++ binary_search.py
@@ -15,6 +15,18 @@ def naive_search(l, target):
 
 # binary search uses divide and conquer!
 # we will leverage the fact that our list is SORTED
-def binary_search(l, target):
+def binary_search(l, target, low=None, high=None):
+    if low is None:
+        low = 0
+    if high is None:
+        high = len(l) - 1
     # example l = [1, 3, 5, 10, 12] # should return 3
     midpoint = (len(l)) // 2 # 2
+
+    if l[midpoint] == target:
+        return midpoint
+    elif target < l[midpoint]:
+        return binary_search(l, target, low, midpoint-1)
+    else:
+        # target > l[midpoint]
+        return binary_search(l, target, midpoint+1, high)
